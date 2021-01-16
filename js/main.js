06a859c28814b0b74bbb1043deb37a30308957a2
Vetal-Lip <53917529+Vetal-Lip@users.noreply.github.com>
2021-01-17 01:29:43 +0300

    add preview The Witcher

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var mySwiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".arrow",
+  },
+  breakpoints: {
+    // when window width is >= 540px
+    540: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".header");
+
+menuButton.addEventListener("click", function () {
+  menuButton.classList.toggle("menu-button-active");
+  menu.classList.toggle("header-active");
+});
